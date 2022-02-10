e7c798fe06c2f4104d94e312ea4c8ca1ad45f8b6
unknown <seancollins117@gmail.com>
2022-02-10 09:54:07 -0800

    still can't get user login to work

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -45,7 +45,7 @@ router.post('/', (req, res) => {
   })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
-      console.log(err);bas
+      console.log(err);
       res.status(500).json(err);
     });
 });
@@ -62,19 +62,13 @@ router.post('/login', (req, res) => {
     }
 
     const validPassword = dbUserData.checkPassword(req.body.password);
-
     if (!validPassword) {
       res.status(400).json({ message: 'Incorrect password!' });
       return;
     }
 
-    // req.session.save(() => {
-    //   req.session.user_id = dbUserData.id;
-    //   req.session.username = dbUserData.username;
-    //   req.session.loggedIn = true;
-  
-      res.json({ user: dbUserData, message: 'You are now logged in!' });
-    });
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+  });
 });
 
 // PUT /api/users/1
