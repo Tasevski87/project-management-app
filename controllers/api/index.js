e4fe16bc7e3a412e96f186116de6ab281f5a3738
toshetasevski <toshetasevski@Toshes-MacBook-Pro.local>
2022-02-10 15:23:46 -0500

    refactor code and comment routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const projectRoutes = require('./project-routes');
 const serviceRoutes = require('./service-routes');
+const commentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);
 router.use('/project', projectRoutes);
 router.use('/service', serviceRoutes);
+router.use('/comment', commentRoutes);
 
 module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { User, Post, Comment, Vote } = require('../../models');
 
-// get all users
+
 // GET /api/users
 router.get('/', (req, res) => {
   // Access our User model and run .findAll() method)
